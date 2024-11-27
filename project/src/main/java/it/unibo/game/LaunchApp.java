edc13db30b5d988244bffa2a46e88743540daeb3
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2024-11-27 21:25:06 +0100

    initialize the repository

---
+++ project/src/main/java/it/unibo/game/LaunchApp.java
@@ -0,0 +1,19 @@
+package it.unibo.game;
+
+/**
+ * Application entry-point.
+ */
+public final class LaunchApp {
+
+    private LaunchApp() {
+    }
+
+    /**
+     * Runs the application.
+     * 
+     * @param args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!"); // NOPMD suppressed as it is a false positive
+    }
+}
---
+++ project/src/test/java/it/unibo/game/TestApp.java
@@ -0,0 +1,25 @@
+package it.unibo.game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Application test.
+ */
+class TestApp {
+
+    /**
+     * Setup function for all test.
+     */
+    @BeforeEach
+    void setup() {
+    }
+
+    /**
+     * Test if application have bug.
+     */
+    @Test
+    void testApp() {
+        // implements test
+    }
+}
